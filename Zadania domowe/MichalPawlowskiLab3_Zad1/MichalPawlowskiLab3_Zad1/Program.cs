ac36d578330b285adae5ba77098d71e3df0b74c1
pawlowskimichal1a <pawlowski.michal1a@gmail.com>
2021-11-28 23:12:36 +0100

    Zadanie domowe 3

---
+++ Zadania domowe/MichalPawlowskiLab3_Zad1/MichalPawlowskiLab3_Zad1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MichalPawlowskiLab3_Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
